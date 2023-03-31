1b9f667ee7943a8aaf1268c761c1270c57b756ff
JayramPai <jayrampai@doshaheen.com>
2023-03-31 23:22:41 +0530

    fixed backend db username

--- frontend/src/app/layout/layout-routing.module.ts
+++ frontend/src/app/layout/layout-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [{
   component: LayoutComponent,
   children: [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', loadChildren: () => import('../modules/Home/home.module').then(m => m.HomeModule) },
+    { path: 'home', loadChildren: () => import('../modules/home/home.module').then(m => m.HomeModule) },
   ]
 }];
 
